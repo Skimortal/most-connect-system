08afe2c6136a6943bb750f7e8e646fe2181140eb
Aleksandar Stojakovic <stojakovic.a@gmail.com>
2025-07-27 18:38:21 +0200

    Cleanup doctrine

--- docker/init.sql
+++ docker/init.sql
@@ -1,43 +1,16 @@
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
---
--- Tabellenstruktur für Tabelle `user`
---
-
-CREATE TABLE IF NOT EXISTS `user` (
-                        `id` int(11) NOT NULL,
-                        `username` varchar(180) NOT NULL,
-                        `roles` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL CHECK (json_valid(`roles`)),
-                        `password` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
---
--- Daten für Tabelle `user`
---
-
-INSERT INTO `user` (`id`, `username`, `roles`, `password`) VALUES
-    (1, 'admin', '[\"ROLE_ADMIN\"]', '$2y$13$SyPxQI1B4D75Kq5x4UN3DedK/YgpMeHsHl3GnOVPWzwjmJiFCP9Xe');
-
---
--- Indizes der exportierten Tabellen
---
-
---
--- Indizes für die Tabelle `user`
---
-ALTER TABLE `user`
-    ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `UNIQ_IDENTIFIER_USERNAME` (`username`);
-
---
--- AUTO_INCREMENT für exportierte Tabellen
---
-
---
--- AUTO_INCREMENT für Tabelle `user`
---
-ALTER TABLE `user`
-    MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-COMMIT;
+INSERT INTO user (
+    company_id, username, roles, password, first_name, last_name,
+    department, position, email, phone_number, mobile_number, fax_number,
+    is_active, created_at, updated_at
+) VALUES (
+             NULL,
+             'admin',
+             '[\"ROLE_SUPERUSER\"]',
+             '\$2y\$13\$SyPxQI1B4D75Kq5x4UN3DedK/YgpMeHsHl3GnOVPWzwjmJiFCP9Xe',
+             'System', 'Administrator',
+             NULL, NULL,
+             'office@most-connect.com',
+             NULL, NULL, NULL,
+             1,
+             NOW(), NOW()
+         );
